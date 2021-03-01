15f70976f547bf9ca256a85c85cba48cd3235478
Santiago Chiesa <chiesa.santiago@gmail.com>
2021-03-01 14:52:16 -0300

    addiing try_me function

---
+++ second_project/lib.py
@@ -0,0 +1,4 @@
+# second_project/lib.py
+
+def try_me():
+    return "This is my second project"
---
+++ tests/lib_test.py
@@ -0,0 +1,5 @@
+# tests/lib_test.py
+from second_project.lib import try_me
+
+def test_length_of_try_me():
+    assert len(try_me()) != 0
